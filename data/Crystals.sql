4c61edef349ccf2bbce6b881e67d4f78d2455f74
yimiu0195 <miruu0195@gmail.com>
2025-08-03 17:18:28 +0700

    update

--- data/Crystals.sql
+++ data/Crystals.sql
@@ -1,11 +1,11 @@
 CREATE TABLE Crystals (
     `id` INT AUTO_INCREMENT PRIMARY KEY,
-    `name` VARCHAR(100),
-    `type` VARCHAR(50),
-    `color` VARCHAR(20),
-    `sell_price` VARCHAR(50),
-    `process_cost` VARCHAR(50),
-    `stats_normal` TEXT,
-    `stats_equipment_limited` TEXT,
-    `obtained_from` TEXT
+    `name` VARCHAR(100) DEFAULT NULL,
+    `type` VARCHAR(50) DEFAULT NULL,
+    `color` VARCHAR(20) DEFAULT NULL,
+    `sell_price` VARCHAR(50) DEFAULT NULL,
+    `process_cost` VARCHAR(50) DEFAULT NULL,
+    `stats_normal` TEXT DEFAULT NULL,
+    `stats_equipment_limited` TEXT DEFAULT NULL,
+    `obtained_from` TEXT DEFAULT NULL
 );
